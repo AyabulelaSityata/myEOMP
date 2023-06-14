6833a320d1b483d05c46633955beef7b0823b8ae
sityata ayabulela <ayabulelasityata97@gmail.com>
2023-06-15 00:55:52 +0200

    home changes

--- script/code.js
+++ script/code.js
@@ -3,6 +3,7 @@ let productList = [
     {
         id: 1,
         title: 'Cargo pants',
+        description: 'ranges from black white all colors',
         image: 'https://i.postimg.cc/0NvPsQpH/55.jpg',
         price: 'R200 - R300'
     },
@@ -10,6 +11,7 @@ let productList = [
     {
         id: 2,
         title: 'Hoodies',
+        description: 'ranges from black white all colors',
         image: 'https://i.postimg.cc/QtQJcCF5/hhhd.jpg',
         price: 'R250 - R450'
     },
@@ -17,6 +19,7 @@ let productList = [
     {
         id: 3,
         title: 'Sneakers',
+        description: 'ranges from black white all colors',
         image: 'https://i.postimg.cc/FsH4LdWX/sneak1.jpg',
         price: 'R1300 - R2500'
     },
@@ -24,6 +27,7 @@ let productList = [
     {
         id: 4,
         title: 'Caps/Beanies',
+        description: 'ranges from black white all colors',
         image: 'https://i.postimg.cc/7ZrPWLhb/cap.jpg',
         price: 'R230 - R380'
     },
@@ -36,6 +40,7 @@ productList.forEach((product) => {
             <img src="${product.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${product.title}</h5>
+                 <p class="card-desc">${product.description}</p>
                 <p class="card-text">${product.price}</p>
                 <a href="./products.html" class="btn"><button>View more</button></a>
             </div>
