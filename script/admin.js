0ede2bef1364c874b5f5f02a3b39e7dd1ce82974
AyabulelaSityata <sityataayabulela@gmail.com>
2023-06-14 16:50:57 +0200

    added changes

---
+++ script/admin.js
@@ -0,0 +1,20 @@
+let products = JSON.stringify []
+
+let product_target = document.querySelector('')
+
+
+let localData = [
+    // insert data here
+    {},
+
+    products.push(localData)
+]
+
+// Set array to localStorage
+
+localStorage.setItem('products', JSON.stringify(products))
+
+
+//
+
+
